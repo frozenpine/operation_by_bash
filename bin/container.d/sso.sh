d6d07d849eed6ca9e5b5e7c1d001b812b8bb8a10
袁暘 <yuanyang@quantdo.com.cn>
2019-04-09 14:30:00 +0800

    修正端口名称

--- bin/container.d/sso.sh
+++ bin/container.d/sso.sh
@@ -60,7 +60,7 @@ docker run -d \
     registry:5000/tradebase/${NAME}:${VERSION} \
         ${JVM_OPTS} \
         -jar /${NAME}/tradebase-${NAME}-${VERSION}.jar \
-        --server.port=${SSO_PORT} \
+        --server.port=${TRADEBASE_PORT} \
         --spring.datasource.url="jdbc:mysql://${MYSQL_HOST}:${MYSQL_PORT}/${DB_NAME}?characterEncoding=utf-8" \
         --spring.datasource.username="${DB_USER}" \
         --spring.datasource.password="${DB_PASS}" \
