41310cfcc00a3a6613d3e8639533a7faacab823a
袁暘 <yuanyang@quantdo.com.cn>
2019-04-19 09:51:18 +0800

    新增security组件

---
+++ bin/container.d/security.sh
@@ -0,0 +1,95 @@
+VERSION=${DIGITAL_SECURITY_VERSION}
+
+if [[ -z ${VERSION} ]]; then
+    error "security version missing."
+    exit 1
+fi
+
+NAME=security
+USER=${NAME}
+
+JVM_OPTS=""
+
+SENTRY_DSN=`get_sentry_dsn ${NAME}`
+
+DB_NAME="sso"
+
+SERVICE_LIST="registry mysql redis digital"
+for SERVICE in ${SERVICE_LIST}; do
+    source "${BASE_DIR}/service.d/${SERVICE}.sh" || {
+        echo "service list file missing: ${SERVICE}.sh" >&2
+        exit 1
+    }
+done
+
+DB_USER=
+DB_PASS=
+for CONF in `extract_ini_sec ${DB_NAME} "${CONF_BASE}/dbs.ini"`; do
+    if [[ $CONF =~ .*[Uu][Ss][Ee][Rr] ]]; then
+        DB_USER=`echo ${CONF} | cut -d'=' -f2`
+        DB_USER=${DB_USER## }
+        DB_USER=${DB_USER%% }
+    fi
+    if [[ $CONF =~ .*[Pp][Aa][Ss][Ss]([Ww][Oo][Rr][Dd])? ]]; then
+        DB_PASS=`echo ${CONF} | cut -d'=' -f2`
+        DB_PASS=${DB_PASS## }
+        DB_PASS=${DB_PASS%% }
+    fi
+done
+
+MYSQL_HOST=
+IDX=$((RANDOM % ${#MYSQL_LIST[@]}))
+COUNT=0
+for SVR_NAME in ${!MYSQL_LIST[@]}; do
+    if [[ ${COUNT} -eq ${IDX} ]]; then
+        MYSQL_HOST=${MYSQL_LIST[$SVR_NAME]}
+        break
+    fi
+
+    COUNT=$((COUNT+1))
+done
+
+REDIS_HOST=
+IDX=$((RANDOM % ${#REDIS_LIST[@]}))
+COUNT=0
+for SVR_NAME in ${!REDIS_LIST[@]}; do
+    if [[ ${COUNT} -eq ${IDX} ]]; then
+        REDIS_HOST=${REDIS_LIST[$SVR_NAME]}
+        break
+    fi
+
+    COUNT=$((COUNT+1))
+done
+
+SELF_IP=`ip address show ${BIND_INT:=eth0} | grep inet | grep -v inet6 | awk '{print $2}' | cut -d'/' -f1`
+
+CONTAINER_BASE="${DATA_BASE:=/opt}/${NAME}"
+find_user ${USER}
+if [[ $? -ne 0 ]]; then
+    ${SUDO} useradd --home-dir "${CONTAINER_BASE}" \
+            --create-home \
+            --shell /sbin/nologin \
+            ${USER} || exit 1
+fi
+make_dir -b "${CONTAINER_BASE}" log || exit 1
+${SUDO} chown -R ${USER}:${USER} "${CONTAINER_BASE}"
+
+docker run -d \
+    --name ${NAME} \
+    --restart no \
+    --network host \
+    --user `grep ${USER} /etc/passwd | cut -d':' -f3` \
+    -e SENTRY_DSN="${SENTRY_DSN}" \
+    registry:5000/digital/${NAME}:${VERSION} \
+        ${JVM_OPTS} \
+        -jar /${NAME}/digital-${NAME}-${VERSION}.jar \
+        --server.port=$((DIGITAL_PORT+1)) \
+        --spring.datasource.url="jdbc:mysql://${MYSQL_HOST}:${MYSQL_PORT}/${DB_NAME}?characterEncoding=utf-8" \
+        --spring.datasource.username="${DB_USER:=$DEFAULT_DB_USER}" \
+        --spring.datasource.password="${DB_PASS:=$DEFAULT_DB_PASS}" \
+        --spring.redis.host=${REDIS_HOST} \
+        --spring.redis.port=${REDIS_PORT} \
+        --dubbo.registry.address="${ZK_SERVERS}" \
+        --dubbo.provider.host="${SELF_IP}" \
+        --dubbo.registry.file="/${NAME}/data/dubbo/dubbo-registry.properties" \
+        --dubbo.provider.timeout=180000
