37dc95ac53746c728e7b7234aff77d78756ebf90
袁暘 <yuanyang@quantdo.com.cn>
2019-04-24 21:14:55 +0800

    新增nexus3镜像

--- bin/module.d/docker.sh
+++ bin/module.d/docker.sh
@@ -1,55 +1,125 @@
+# checking container's running status.
+# position args: $1  container name
+# return code:   0   running
+#                1   stopped
+#                255 container missing
 function check_container() {
     if [[ $# -lt 1 ]]; then
         error "container name missing in check."
         exit 1
     fi
 
-    local _err=0
+    local _name=$1
 
-    for CONTAINER in $*; do
-        COUNT=`docker ps --filter=status=running | awk -vNAME=${CONTAINER} 'FNR!=1{ if($NF==NAME)print}' | wc -l`
-        
-        if [[ $COUNT -eq 1 ]]; then
-            info "docker container[${CONTAINER}] is running."
-        else
-            docker ps -a | grep ${CONTAINER} &>/dev/null
-            if [[ $? -ne 0 ]]; then
-                error "docker container[${CONTAINER}] missing."
-            else
-                warning "docker container[${CONTAINER}] not running."
-            fi
-            _err=$((_err+1))
-        fi
-    done
+    COUNT=`docker ps --filter=status=running | awk -vNAME=${_name} 'FNR!=1{ if($NF==NAME)print}' | wc -l`
+    
+    if [[ $COUNT -eq 1 ]]; then
+        info "container[${_name}] is running."
+        return
+    fi
 
-    return ${_err}
+    docker ps -a | grep ${CONTAINER} &>/dev/null
+    if [[ $? -ne 0 ]]; then
+        error "container[${CONTAINER}] missing."
+        return 255
+    else
+        warning "container[${CONTAINER}] not running."
+        return 1
+    fi
 }
 
+# start/create container
+# args:   -w {pos-integer}  waiting {pos-integer}(default: 1) seconds & check container status again
+# position args: $1         container name
+#                $*         extra args for start script
+# return code:   0          started
+#                1          already started
+#                2          start failed
+#                255        start script missing
 function start_container() {
     if [[ $# -lt 1 ]]; then
         error "container name missing in start."
         exit 1
     fi
 
-    local _container=$1
-    local _count=`docker ps --all --filter=name=${_container} --quiet | wc -l`
-    local _start_script=`find "${CONTAINER_BASE}" -type f -name "${_container}.sh"`
+    local _wait=1
 
-    if [[ ${_count} -eq 1 ]]; then
-        docker start ${_container}
-        return $?
-    else
-        if [[ -f "${_start_script}" ]]; then
+    local OPTIND FLAG
+    while getopts :w: FLAG; do
+        case $FLAG in
+            w)
+                _wait=${OPTARG}
+
+                if [[ ! ${_wait} =~ [0-9]+ ]]; then
+                    error "wait args must be a positive integer."
+                    exit 1
+                fi
+            ;;
+            *)
+                error "invalid start container args: $*"
+                exit 1
+            ;;
+        esac
+    done
+    shift $((OPTIND-1))
+
+    local _name=$1
+    local _result
+    
+    check_container ${_name}
+    _result=$?
+
+    case ${_status} in
+        0)
+            warning "container[${_name}] is already started."
+            return 1
+        ;;
+        1)
+            info "starting container[${_name}]..."
+            
+            docker start ${_name}
+            
+            if [[ $? -ne 0 ]]; then
+                error "starting container[${_name}] failed."
+                return 2
+            fi
+        ;;
+        255)
+            local _start_script=`find "${CONTAINER_BASE}" -type f -name "${_name}.sh"`
+            
+            if [[ ! -f "${_start_script}" ]]; then
+                error "no start script found for ${_container}"
+                return 255
+            fi
+
+            info "creating container[${_name}]..."
+
+            # shift container name
             shift
             source "${_start_script}" $*
-            return $?
-        else
-            error "no start script found for ${_container}"
-            return 1
-        fi
-    fi
+
+            if [[ $? -ne 0 ]]; then
+                error "creating container[${_name}] failed."
+                return 2
+            fi
+        ;;
+        *)
+            error ""
+        ;;
+    esac
+
+    sleep ${_wait}
+    check_container ${_name} || return 2
 }
 
+# stop, remove container & clean it's data volumes
+# args:   -f        stop forcely
+#         -r        remove container
+#         -c        clean data volume
+#         -s        skip backup while cleanning data volume
+# position args: $1 container name
+# return code:   0  stopped
+#                1  failed while stopping, removeing or cleanning
 function stop_container() {
     if [[ $# -lt 1 ]]; then
         error "container name missing in stop."
@@ -84,43 +154,46 @@ function stop_container() {
     done
     shift $((OPTIND-1))
 
+    local _name=$1
+
     case "${_force}${_remove}" in
         "11")
             docker rm -vf $1
+
             if [[ $? -ne 0 ]]; then
-                error "failed to kill & remove container[$1]"
+                error "failed to kill & remove container[${_name}]"
                 return 1
             fi
         ;;
         "01")
             docker stop $1
             if [[ $? -ne 0 ]]; then
-                error "failed to stop container[$1]"
+                error "failed to stop container[${_name}]"
                 return 1
             fi
             docker rm -v $1
             if [[ $? -ne 0 ]]; then
-                error "failed to remove container[$1]"
+                error "failed to remove container[${_name}]"
                 return 1
             fi
         ;;
         "10")
             docker kill $1
             if [[ $? -ne 0 ]]; then
-                error "failed to kill container[$1]"
+                error "failed to kill container[${_name}]"
                 return 1
             fi
         ;;
         "00")
             docker stop $1
             if [[ $? -ne 0 ]]; then
-                error "failed to stop container[$1]"
+                error "failed to stop container[${_name}]"
                 return 1
             fi
         ;;
         *)
             error "invalid stop | remove args."
-            return 1
+            exit 1
         ;;
     esac
 
@@ -129,13 +202,13 @@ function stop_container() {
         local _back_base="${DATA_BASE:=/opt}/backup"
         local _back_file="${_back_base}/$1_`date '+%Y%m%d%H%M%S'`.tar.gz"
 
-        if [[ ${SKIP_BACK} -ne 1 ]]; then
+        if [[ ${_skip_back} -ne 1 ]]; then
             info "backing up container[$1] data dir: ${_data_dir}"
             if [[ ! -d "${_back_base}" ]]; then
                 ${SUDO} mkdir -p "${_back_base}"
             fi
             if [[ ! -d "${_data_dir}" ]]; then
-                warning "data dir not exist for container[$1]."
+                warning "data dir not exist for container[${_name}]."
             else
                 pushd "${_data_dir}" >/dev/null
                     ${SUDO} tar -czvf "${_back_file}" ./
@@ -145,7 +218,7 @@ function stop_container() {
         fi
 
         if [[ -d "${_data_dir}" ]]; then
-            info "cleaning container[$1] data dir: ${_data_dir}"
+            info "cleaning container[${_name}] data dir: ${_data_dir}"
             ${SUDO} rm -rf "${_data_dir}"
         fi
     fi
