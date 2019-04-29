7c7d2a6afbd23099f6604499d9a332cea9613da9
袁暘 <yuanyang@quantdo.com.cn>
2019-04-29 19:35:36 +0800

    修正es单节点端口错误

--- bin/container.d/elastic.sh
+++ bin/container.d/elastic.sh
@@ -62,7 +62,7 @@ else
         -v "${CONTAINER_BASE}/data:/usr/share/elasticsearch/data" \
         -e "cluster.name=${CLUSTER_NAME}" \
         -e "discovery.type=single-node" \
-        -e "http.port=${ES_PORT}" \
-        -e "transport.tcp.port=$((ES_PORT+100))" \
+        -e "http.port=${ELASTIC_PORT}" \
+        -e "transport.tcp.port=$((ELASTIC_PORT+100))" \
         registry:5000/${NAME}:${VERSION}
 fi
