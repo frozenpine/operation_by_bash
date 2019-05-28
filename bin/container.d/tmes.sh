5569f62e72d16b66718901ce756ad075bbbbe1d1
袁暘 <yuanyang@quantdo.com.cn>
2019-05-28 11:31:11 +0800

    新增组件

--- bin/container.d/tmes.sh
+++ bin/container.d/tmes.sh
@@ -1,20 +1,20 @@
-VERSION=${TRADE_TMDB_VERSION}
+VERSION=${TRADE_TMES_VERSION}
 
 if [[ -z ${VERSION} ]]; then
-    error "tmdb version missing."
+    error "tmes version missing."
     exit 1
 fi
 
-NAME=tmdb
+NAME=tmes
 USER=${NAME}
 
-JVM_OPTS=${TRADE_TMDB_JVM}
+JVM_OPTS=${TRADE_TMES_JVM}
 
 SENTRY_DSN=`get_sentry_dsn ${NAME}`
 
 DB_NAME="clear"
 
-SERVICE_LIST="registry zookeeper kafka mysql tmdb"
+SERVICE_LIST="registry zookeeper kafka elastic tmes"
 for SERVICE in ${SERVICE_LIST}; do
     source "${BASE_DIR}/service.d/${SERVICE}.sh" || {
         echo "service list file missing: ${SERVICE}.sh" >&2
@@ -22,39 +22,17 @@ for SERVICE in ${SERVICE_LIST}; do
     }
 done
 
-DB_USER=
-DB_PASS=
-for CONF in `extract_ini_sec ${DB_NAME} "${CONF_BASE}/dbs.ini"`; do
-    if [[ $CONF =~ .*[Uu][Ss][Ee][Rr] ]]; then
-        DB_USER=`echo ${CONF} | cut -d'=' -f2`
-        DB_USER=${DB_USER## }
-        DB_USER=${DB_USER%% }
-    fi
-    if [[ $CONF =~ .*[Pp][Aa][Ss][Ss]([Ww][Oo][Rr][Dd])? ]]; then
-        DB_PASS=`echo ${CONF} | cut -d'=' -f2`
-        DB_PASS=${DB_PASS## }
-        DB_PASS=${DB_PASS%% }
-    fi
-done
-
-MYSQL_HOST=
-# IDX=$((RANDOM % ${#MYSQL_LIST[@]}))
-IDX=0
-COUNT=0
-for SVR_NAME in ${!MYSQL_LIST[@]}; do
-    if [[ ${COUNT} -eq ${IDX} ]]; then
-        MYSQL_HOST=${MYSQL_LIST[$SVR_NAME]}
-        break
-    fi
-
-    COUNT=$((COUNT+1))
-done
-
 KAFKA_SERVERS=
 for SVR_NAME in ${!KAFKA_LIST[@]}; do
     KAFKA_SERVERS="${KAFKA_SERVERS},${SVR_NAME}:${KAFKA_PORT}"
 done
 KAFKA_SERVERS=${KAFKA_SERVERS:1}
+
+ELASTIC_SERVERS=
+for SVR_NAME in ${!ELASTIC_LIST[@]}; do
+    ELASTIC_SERVERS="${ELASTIC_SERVERS},http://${SVR_NAME}:${ELASTIC_PORT}"
+done
+ELASTIC_SERVERS=${ELASTIC_SERVERS:1}
 
 SELF_IP=`ip address show eth0 | grep inet | grep -v inet6 | awk '{print $2}' | cut -d'/' -f1`
 
@@ -82,10 +60,8 @@ docker run -d \
         -jar /${NAME}/trade$1-${NAME}-${VERSION}.jar \
         --logging.level.com.quantdo.trade=${LOG_LEVEL:=info} \
         --server.address=${SELF_IP} \
-        --server.port=${TMDB_PORT} \
-        --spring.datasource.url="jdbc:mysql://${MYSQL_HOST}:${MYSQL_PORT}/${DB_NAME}?autoReconnect=true&useUnicode=true&characterEncoding=utf-8&connectionCollation=utf8_general_ci&useSSL=false&serverTimezone=Asia/Shanghai" \
-        --spring.datasource.username=${DB_USER:=$DEFAULT_DB_USER} \
-        --spring.datasource.password=${DB_PASS:=$DEFAULT_DB_PASS} \
+        --server.port=${TMES_PORT} \
+        --spring.data.elasticsearch.cluster-nodes=${ELASTIC_SERVERS} \
         --com.quantdo.trade.sink.properties.bootstrap.servers=${KAFKA_SERVERS} \
         --com.quantdo.trade.sink.source-topic=${SINK_TOPIC:DBTOPIC} \
         --com.quantdo.trade.sink.properties.fetch.max.wait.ms=10 \
